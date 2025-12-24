333a35b591312e265cf3deb034d2341a3d3e8d3a
Reza Fauzan Adhima <rezafauzan593@gmail.com>
2025-12-24 10:46:42 +0700

    add js script to calculate circumference and area of a circle

---
+++ task1.js
@@ -0,0 +1,9 @@
+let jariJari = 14
+let pi = 22/7
+let luas, keliling
+
+keliling = 2 * pi * jariJari
+luas = pi * jariJari * jariJari
+
+console.log(`Keliling dari lingkaran dengan jari-jari = ${jariJari} adalah ${keliling}`);
+console.log(`Luas dari lingkaran dengan jari-jari = ${jariJari} adalah ${luas}`);
